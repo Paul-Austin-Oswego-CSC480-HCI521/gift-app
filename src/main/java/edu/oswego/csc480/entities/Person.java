213c74367728a8c5e9fd00b4c1d128805e6f5746
Andrew F. Kerfien Torres <akerfien@oswego.edu>
2026-09-20 21:00:18 -0400

    feature: work on person class. not finished, work depends on other classes.

--- src/main/java/edu/oswego/csc480/entities/Person.java
+++ src/main/java/edu/oswego/csc480/entities/Person.java
@@ -1,8 +1,65 @@
 package edu.oswego.csc480.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="person")
 public class Person {
-    // TODO
+
+    @Column(name="person_id")
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="user_id")
+    @ManyToOne
+    private User user;
+
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="full_name")
+    private String fullName;
+
+    public Person(){}
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
