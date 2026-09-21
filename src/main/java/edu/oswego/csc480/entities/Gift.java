b8ba9df950b05691aaf8723b810d13083bd18671
Andrew F. Kerfien Torres <akerfien@oswego.edu>
2026-09-20 22:23:27 -0400

    started work on Gift class. A LOT of TODOs.

--- src/main/java/edu/oswego/csc480/entities/Gift.java
+++ src/main/java/edu/oswego/csc480/entities/Gift.java
@@ -1,8 +1,34 @@
 package edu.oswego.csc480.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
 
 @Entity
+@Table(name = "gift")
 public class Gift {
-    // TODO
+
+    // TODO : finish this class and its relation to: Person && Occasion.
+
+    private Integer id;
+
+    private Person person;
+    private Occasion occasion;
+    //TODO : gift_cycle && relation to it
+    private LocalDate purchaseDate;
+    private String name;
+    //TODO: price, but i ain't touching it as database defined with MONEY type.
+    private String type;
+
+    public Gift(){
+
+    }
+
+    //TODO: add setters and getters
+
+
+
+
+
 }
