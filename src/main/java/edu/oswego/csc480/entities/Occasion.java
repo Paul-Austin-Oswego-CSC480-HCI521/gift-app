4c93438a581a1fe18ef1d3c1e52356f488b75158
Andrew F. Kerfien Torres <akerfien@oswego.edu>
2026-09-20 22:09:48 -0400

    started work on Occasion. TODO gift and its relation to Person and Occasion.

--- src/main/java/edu/oswego/csc480/entities/Occasion.java
+++ src/main/java/edu/oswego/csc480/entities/Occasion.java
@@ -1,8 +1,46 @@
 package edu.oswego.csc480.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
+@Table(name="occasion")
 public class Occasion {
-    // TODO
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "occasion_id")
+    private Integer id;
+
+    @Column(name = "occasion_name")
+    private String name;
+
+    @Column(name = "occasion_date")
+    private LocalDate date;
+
+    public Occasion(){}
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
 }
